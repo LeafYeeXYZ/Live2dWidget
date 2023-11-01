f8bb864cbf28da4a893c4ec3b2fc44474044ae91
LeafYeeXYZ <leafyeexyz@outlook.com>
2023-11-01 13:05:28 +0800

    1

--- autoload.js
+++ autoload.js
@@ -36,7 +36,8 @@ if (screen.width >= 768) {
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
 			cdnPath: "https://live2d-api-4vv.pages.dev/",
-			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
+			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "quit"]
+			//tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
 }
