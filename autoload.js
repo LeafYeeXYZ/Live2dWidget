ee265447bc0f03b581ea2c89506130d86ca8ae8a
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-04-10 12:04:11 +0800

    up

--- autoload.js
+++ autoload.js
@@ -1,3 +1,7 @@
+// live2d_path 参数建议使用绝对路径
+const live2d_path = "https://live2dwidget.leafyee.xyz/";
+//const live2d_path = "/live2d-widget/";
+
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
 	return new Promise((resolve, reject) => {
@@ -23,15 +27,15 @@ function loadExternalResource(url, type) {
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
 	Promise.all([
-		loadExternalResource("/waifu.css", "css"),
-		loadExternalResource("/live2d.min.js", "js"),
-		loadExternalResource("/waifu-tips.js", "js")
+		loadExternalResource(live2d_path + "waifu.css", "css"),
+		loadExternalResource(live2d_path + "live2d.min.js", "js"),
+		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
 		// 配置选项的具体用法见 README.md
 		initWidget({
-			waifuPath: "/waifu-tips.json",
+			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "/",
+			cdnPath: live2d_path,
 			//tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 			tools: ["asteroids", "switch-model", "photo", "quit"]
 		});
