65fdfeef46b9d8c00ced47aba75d3163eeff4cce
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-05-27 19:53:40 +0800

    up

--- src/client.ts
+++ src/client.ts
@@ -11,8 +11,13 @@ window.initWidget = initWidget
 
 // 从当前 <script> 标签获取基础路径
 const script = document.currentScript
-const BASE_URL = script ? script.getAttribute('data-server')! : 'https://live2dwidget.leafyee.xyz/'
-const TOOLS = script ? script.getAttribute('data-tools')!.split(',') : ['hitokoto', 'switch-model', 'switch-texture', 'photo', 'info', 'quit']
+let BASE_URL = 'https://live2dwidget.leafyee.xyz/'
+let TOOLS = 'hitokoto,switch-model,switch-texture,photo,info,quit'
+if (script) {
+    BASE_URL = script.getAttribute('data-server') || BASE_URL
+    TOOLS = script.getAttribute('data-tools') || TOOLS
+}
+const tools = TOOLS.split(',').map(tool => tool.trim()) as ('hitokoto' | 'switch-model' | 'switch-texture' | 'photo' | 'info' | 'quit')[]
 // 将 waifu.css 加载到页面中
 const waifuCss = document.createElement('link')
 waifuCss.rel = 'stylesheet'
@@ -21,5 +26,5 @@ document.head.appendChild(waifuCss)
 // 加载看板娘
 screen.width >= 768 && initWidget({
     BASE_URL: BASE_URL,
-    TOOLS: TOOLS as ('hitokoto' | 'switch-model' | 'switch-texture' | 'photo' | 'info' | 'quit')[]
+    TOOLS: tools
 })
